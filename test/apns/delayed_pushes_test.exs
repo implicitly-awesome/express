87c07d0096225d455b51a263fa9540dc76665afa
Andrei Chernykh <andrei.chernykh@gmail.com>
2017-10-25 17:46:12 +0300

    APNS.DelayedPushesTest

---
+++ test/apns/delayed_pushes_test.exs
@@ -0,0 +1,21 @@
+defmodule APNS.DelayedPushesTest do
+  @moduledoc false
+
+  alias Express.APNS.DelayedPushes
+  alias Express.APNS.PushMessage
+
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, sup_pid} = DelayedPushes.start_link()
+
+    {:ok, %{sup_pid: sup_pid}}
+  end
+
+  test "add/3: spawns a buffer adder", %{sup_pid: sup_pid} do
+    DelayedPushes.add(%PushMessage{}, [delay: 3], fn(_, _) -> :ok end)
+    DelayedPushes.add(%PushMessage{}, [delay: 3], fn(_, _) -> :ok end)
+
+    assert %{active: 2} = Supervisor.count_children(sup_pid)
+  end
+end
